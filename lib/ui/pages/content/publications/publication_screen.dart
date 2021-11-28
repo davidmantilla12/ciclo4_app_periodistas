ea92511983564816de7e5bdf23c88318d751a64b
HanzDa <hdgl.9914@gmail.com>
2021-11-28 16:42:24 -0500

    new post view added to the project

--- lib/ui/pages/content/publications/publication_screen.dart
+++ lib/ui/pages/content/publications/publication_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:red_periodistas/ui/pages/content/publications/widgets/add_new_post.dart';
 import 'package:red_periodistas/ui/pages/content/publications/widgets/post_card.dart';
-import 'package:red_periodistas/ui/theme/colors.dart';
 
 class PublicOffersScreen extends StatefulWidget {
   // PublicOffersScreen empty constructor
@@ -16,41 +16,44 @@ class _State extends State<PublicOffersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return OfferCard(
-            title:
-                'Conozca los nuevos tiempos de la convocatoria para el programa “Misión TIC 2022”',
-            content:
-                'El Ministerio de Tecnologías de la Información y las Comunicaciones, informa a todos los interesados en participar de la convocatoria de la ruta de aprendizaje 1 y 2, la modificación del cronograma.',
-            source: 'Emisora Ondas de Ibagué',
-            fecha: "20/11/2021",
-            enlace:
-                "http://www.ondasdeibague.com/noticias/nacionales/40422-conozca-los-nuevos-tiempos-de-la-convocatoria-para-el-programa-mision-tic-2022",
-            onCopy: () => {},
-            onApply: () => {},
-          );
-        },
-      ),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return OfferCard(
+              title:
+                  'Conozca los nuevos tiempos de la convocatoria para el programa “Misión TIC 2022”',
+              content:
+                  'El Ministerio de Tecnologías de la Información y las Comunicaciones, informa a todos los interesados en participar de la convocatoria de la ruta de aprendizaje 1 y 2, la modificación del cronograma.',
+              source: 'Emisora Ondas de Ibagué',
+              fecha: "20/11/2021",
+              enlace:
+                  "http://www.ondasdeibague.com/noticias/nacionales/40422-conozca-los-nuevos-tiempos-de-la-convocatoria-para-el-programa-mision-tic-2022",
+              onCopy: () => {},
+              onApply: () => {},
+            );
+          },
+        ),
 
-      floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: Colors.green[600],
-        icon: const Icon(
-          Icons.add_circle_outline_sharp,
-          size: 50.0,
-          color: Colors.white,
-          ),  
-      
-        label: const Text('Nueva\nPublicación',
+        // add a new post
+        floatingActionButton: FloatingActionButton.extended(
+          backgroundColor: Colors.green[600],
+          icon: const Icon(
+            Icons.add_circle_outline_sharp,
+            size: 50.0,
+            color: Colors.white,
+          ),
+          label: const Text(
+            'Nueva\nPublicación',
             textAlign: TextAlign.center,
             style: TextStyle(
               fontWeight: FontWeight.bold,
               color: Colors.white70,
             ),
           ),
-        onPressed: () {},) 
-
-    );
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => NewPost()));
+          },
+        ));
   }
 }
---
+++ lib/ui/pages/content/publications/widgets/add_new_post.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class NewPost extends StatefulWidget {
+  const NewPost({Key? key}) : super(key: key);
+
+  @override
+  _NewPostState createState() => _NewPostState();
+}
+
+class _NewPostState extends State<NewPost> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: get and handle data from new post view
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Nueva publicación',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+        centerTitle: true,
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+        child: Column(
+          children: [
+            // post title
+            TextFormField(
+              autofocus: true,
+              maxLength: 100,
+              keyboardType: TextInputType.multiline,
+              decoration: const InputDecoration(
+                  label: Text('Título'),
+                  labelStyle: TextStyle(
+                    fontSize: 25.0,
+                  ),
+                  hintText: 'Agrega el título de tu post',
+                  hintStyle: TextStyle(color: Colors.grey)),
+            ),
+
+            const SizedBox(height: 20.0),
+            // post abstract
+            TextFormField(
+              autocorrect: true,
+              maxLines: 7,
+              maxLength: 500,
+              keyboardType: TextInputType.multiline,
+              decoration: const InputDecoration(
+                  label: Text('Resumen'),
+                  labelStyle: TextStyle(
+                    fontSize: 25.0,
+                  ),
+                  hintText: 'Agrega una breve descripción de la notícia',
+                  hintStyle: TextStyle(color: Colors.grey)),
+            ),
+
+            // post category
+            const SizedBox(height: 20.0),
+            TextFormField(
+              autofocus: true,
+              maxLength: 50,
+              keyboardType: TextInputType.multiline,
+              decoration: const InputDecoration(
+                  label: Text('Categoria'),
+                  labelStyle: TextStyle(
+                    fontSize: 25.0,
+                  ),
+                  hintText: 'Ej: deportes, tecnología, politica, etc',
+                  hintStyle: TextStyle(color: Colors.grey)),
+            ),
+
+            // post sub-category
+            const SizedBox(height: 20.0),
+            TextFormField(
+              autofocus: true,
+              maxLength: 50,
+              keyboardType: TextInputType.multiline,
+              decoration: const InputDecoration(
+                  label: Text('Sub-Categoria'),
+                  labelStyle: TextStyle(
+                    fontSize: 25.0,
+                  ),
+                  hintText: 'Ej: electronica, espacio, IA, etc',
+                  hintStyle: TextStyle(color: Colors.grey)),
+            ),
+
+            const SizedBox(height: 35.0),
+            FloatingActionButton.extended(
+              backgroundColor: Colors.green[600],
+              icon: const Icon(
+                Icons.send_rounded,
+                size: 50.0,
+                color: Colors.white,
+              ),
+              label: const Text(
+                'Publicar',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white70,
+                ),
+              ),
+
+              // TODO: implement add category controller
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
