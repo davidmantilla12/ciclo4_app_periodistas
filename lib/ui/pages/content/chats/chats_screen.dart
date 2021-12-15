2cb912186e9bff2fe07b7b44709c3cf2b1321911
HanzDa <hdgl.9914@gmail.com>
2021-12-15 06:54:53 -0500

    working in chats view

--- lib/ui/pages/content/chats/chats_screen.dart
+++ lib/ui/pages/content/chats/chats_screen.dart
@@ -12,7 +12,7 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _State extends State<ChatsScreen> {
-  final items = List<String>.generate(2, (i) => "Item $i");
+  final items = List<String>.generate(21, (i) => "Item $i");
 
   @override
   Widget build(BuildContext context) {
@@ -26,16 +26,46 @@ class _State extends State<ChatsScreen> {
           controladorChats.cantmensajes.toString() +
           " Mensajes sin leer)";
     }
-    return ListView.builder(
+    return Scaffold(
+      body: ListView.builder(
       itemCount: items.length,
       itemBuilder: (context, index) {
         return StateCard(
           title: textoChat,
-          content: 'Lorem ipsum dolor sit amet.',
+          content: 'hi I am a journalist for the Circilo de periodistas'
+                   'valledupar and I love using Query News',
+                   
           picUrl: 'https://uifaces.co/our-content/donated/gPZwCbdS.jpg',
           onChat: () => {},
         );
       },
+    ),
+      bottomSheet: Row(
+        children: [
+          Container(
+            height: 50.0,
+            width: 400.0,
+            child: TextFormField(
+              style: const TextStyle(
+                fontSize: 18.0,
+              ),
+              autofocus: true,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(
+                  Icons.chat_outlined,
+                  color: Colors.orangeAccent,
+                  
+                  ),
+                hintText: 'chatea en vivo',
+                border: UnderlineInputBorder(),
+                
+              ),
+            ),
+          ),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.send,size: 40.0,)),
+        ],
+      ),
+
     );
   }
 }
