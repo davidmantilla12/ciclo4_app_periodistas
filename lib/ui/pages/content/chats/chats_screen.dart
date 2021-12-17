8a0512a0ed843d666e951d598d761df49810df62
HanzDa <hdgl.9914@gmail.com>
2021-12-17 13:56:33 -0500

    changing chat field size

--- lib/ui/pages/content/chats/chats_screen.dart
+++ lib/ui/pages/content/chats/chats_screen.dart
@@ -43,7 +43,7 @@ class _State extends State<ChatsScreen> {
       bottomSheet: Row(
         children: [
           SizedBox(
-            width: 400.0,
+            width: 310.0,
             child: TextFormField(
               minLines: 1,
               maxLines: 5,
